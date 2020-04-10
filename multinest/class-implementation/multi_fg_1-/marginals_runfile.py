c38167c841a2037b93fcb76cc145a22590ef7a19
Madalina Tudorache <40742965+mnt116@users.noreply.github.com>
2020-04-10 14:22:09 +0100

    one window foreground models files

---
+++ multinest/class-implementation/multi_fg_1-/marginals_runfile.py
@@ -0,0 +1,31 @@
+# a script to produce marginalised parameters
+
+# importing modules
+from __future__ import absolute_import, unicode_literals, print_function
+from pymultinest.solve import solve
+from pymultinest.analyse import Analyzer
+import os
+import sys
+import shutil
+import plot
+import matplotlib.pyplot as plt
+try: os.mkdir('chains')
+except OSError: pass
+
+do_pretty = True
+
+n_params = 8
+
+prefix = 'multi_fg_1-'
+
+an = Analyzer(n_params, outputfiles_basename=prefix)
+for i in range(n_params):
+    plt.figure()
+    marge = plot.PlotMarginalModes(an)
+    if do_pretty == True:
+        out = marge.plot_modes_marginal(i,with_ellipses=False)
+        plt.savefig("parameter%i_marginal.png"%i)
+    else:
+        out = marge.plot_conditional(i,with_ellipses=False)
+        plt.savefig("posterior%i_marginal.png"%i)
+
